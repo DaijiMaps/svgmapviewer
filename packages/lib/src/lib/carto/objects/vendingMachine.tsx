9b6432b005c5ef6665dc8510fb4ab879e3e6c19e
Masao Uebayashi <uebayasi@tombiinc.com>
2025-04-26 02:21:02 +0900

    close paths

--- packages/lib/src/lib/carto/objects/vendingMachine.tsx
+++ packages/lib/src/lib/carto/objects/vendingMachine.tsx
@@ -11,4 +11,4 @@ export function VendingMachine() {
 }
 
 export const vendingMachinePath =
-  'm -0.5,0 v -1.8 h 1 v 1.8 h -1 m 0.1,-0.9 v -0.7 h 0.8 v 0.7 h -0.8 m 0.6,0.2 h 0.2 m -0.7,0.5 v -0.2 h 0.6 v 0.2 h -0.6'
+  'm -0.5,0 v -1.8 h 1 v 1.8 z m 0.1,-0.9 v -0.7 h 0.8 v 0.7 z m 0.6,0.2 h 0.2 m -0.7,0.5 v -0.2 h 0.6 v 0.2 z'
