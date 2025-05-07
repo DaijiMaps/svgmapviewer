d421b2fb3002dc0e263828a369598a28b081bdce
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-07 15:59:11 +0900

    format

--- packages/app/vite.config-local.ts
+++ packages/app/vite.config-local.ts
@@ -1,6 +1,6 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 import path from 'node:path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,16 +8,46 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@daijimaps/svgmapviewer/box": path.resolve(__dirname, '../lib/src/lib/box/prefixed/index.ts'),
-      "@daijimaps/svgmapviewer/carto": path.resolve(__dirname, '../lib/src/lib/carto/index.ts'),
-      "@daijimaps/svgmapviewer/carto-objects": path.resolve(__dirname, '../lib/src/lib/carto/objects/index.ts'),
-      "@daijimaps/svgmapviewer/carto-symbols": path.resolve(__dirname, '../lib/src/lib/carto/symbols/index.ts'),
-      "@daijimaps/svgmapviewer/geo": path.resolve(__dirname, '../lib/src/lib/geo/index.ts'),
-      "@daijimaps/svgmapviewer/matrix": path.resolve(__dirname, '../lib/src/lib/matrix/prefixed/index.ts'),
-      "@daijimaps/svgmapviewer/search": path.resolve(__dirname, '../lib/src/lib/search/index.ts'),
-      "@daijimaps/svgmapviewer/tuple": path.resolve(__dirname, '../lib/src/lib/tuple/index.ts'),
-      "@daijimaps/svgmapviewer/vec": path.resolve(__dirname, '../lib/src/lib/vec/prefixed/index.ts'),
-      "@daijimaps/svgmapviewer": path.resolve(__dirname, '../lib/src/lib/index.ts'),
-    }
-  }
+      '@daijimaps/svgmapviewer/box': path.resolve(
+        __dirname,
+        '../lib/src/lib/box/prefixed/index.ts'
+      ),
+      '@daijimaps/svgmapviewer/carto': path.resolve(
+        __dirname,
+        '../lib/src/lib/carto/index.ts'
+      ),
+      '@daijimaps/svgmapviewer/carto-objects': path.resolve(
+        __dirname,
+        '../lib/src/lib/carto/objects/index.ts'
+      ),
+      '@daijimaps/svgmapviewer/carto-symbols': path.resolve(
+        __dirname,
+        '../lib/src/lib/carto/symbols/index.ts'
+      ),
+      '@daijimaps/svgmapviewer/geo': path.resolve(
+        __dirname,
+        '../lib/src/lib/geo/index.ts'
+      ),
+      '@daijimaps/svgmapviewer/matrix': path.resolve(
+        __dirname,
+        '../lib/src/lib/matrix/prefixed/index.ts'
+      ),
+      '@daijimaps/svgmapviewer/search': path.resolve(
+        __dirname,
+        '../lib/src/lib/search/index.ts'
+      ),
+      '@daijimaps/svgmapviewer/tuple': path.resolve(
+        __dirname,
+        '../lib/src/lib/tuple/index.ts'
+      ),
+      '@daijimaps/svgmapviewer/vec': path.resolve(
+        __dirname,
+        '../lib/src/lib/vec/prefixed/index.ts'
+      ),
+      '@daijimaps/svgmapviewer': path.resolve(
+        __dirname,
+        '../lib/src/lib/index.ts'
+      ),
+    },
+  },
 })
