a4e4fdc25612f55aa7575cc5938641b249f5ea77
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-08 16:21:59 +0900

    refactor

--- packages/lib/src/Container.tsx
+++ packages/lib/src/Container.tsx
@@ -1,5 +1,13 @@
 import { forwardRef, PropsWithChildren } from 'react'
 import './Container.css'
+import { PointerRef } from './lib/pointer-xstate'
+import {
+  useDragStyle,
+  useModeStyle,
+  useMoveStyle,
+  useScrollStyle,
+  useZoomStyle,
+} from './lib/style'
 
 export const Container = forwardRef<HTMLDivElement, PropsWithChildren>(
   (props, ref) => {
@@ -10,3 +18,23 @@ export const Container = forwardRef<HTMLDivElement, PropsWithChildren>(
     )
   }
 )
+
+export function ContainerStyle(props: Readonly<{ _pointerRef: PointerRef }>) {
+  const { _pointerRef: pointerRef } = props
+
+  const moveStyle = useMoveStyle(pointerRef)
+  const zoomStyle = useZoomStyle(pointerRef)
+  const scrollStyle = useScrollStyle(pointerRef)
+  const modeStyle = useModeStyle(pointerRef)
+  const dragStyle = useDragStyle(pointerRef)
+
+  return (
+    <style>
+      {scrollStyle}
+      {modeStyle}
+      {dragStyle}
+      {moveStyle}
+      {zoomStyle}
+    </style>
+  )
+}
--- packages/lib/src/Viewer.tsx
+++ packages/lib/src/Viewer.tsx
@@ -1,18 +1,11 @@
 import { PropsWithChildren, useRef } from 'react'
-import { Container } from './Container'
+import { Container, ContainerStyle } from './Container'
 import { Debug } from './Debug'
 import { Detail } from './Detail'
 import { Footer, FooterStyle } from './Footer'
 import { Header, HeaderStyle } from './Header'
 import { usePointer } from './lib/pointer-react'
 import { searchRef } from './lib/search'
-import {
-  useDragStyle,
-  useModeStyle,
-  useMoveStyle,
-  useScrollStyle,
-  useZoomStyle,
-} from './lib/style'
 import { useUi } from './lib/ui-react'
 import { MapHtml } from './MapHtml'
 import { MapSvg } from './MapSvg'
@@ -26,12 +19,6 @@ export const Viewer = (props: Readonly<PropsWithChildren>) => {
 
   const { uiRef } = useUi(pointerRef)
 
-  const moveStyle = useMoveStyle(pointerRef)
-  const zoomStyle = useZoomStyle(pointerRef)
-  const scrollStyle = useScrollStyle(pointerRef)
-  const modeStyle = useModeStyle(pointerRef)
-  const dragStyle = useDragStyle(pointerRef)
-
   return (
     <>
       <Container ref={containerRef}>
@@ -41,25 +28,23 @@ export const Viewer = (props: Readonly<PropsWithChildren>) => {
         <ShadowStyle _uiRef={uiRef} />
         <Detail _pointerRef={pointerRef} _uiRef={uiRef} />
       </Container>
+      <ContainerStyle _pointerRef={pointerRef} />
+
       <Header _uiRef={uiRef} _pointerRef={pointerRef} />
       <HeaderStyle _uiRef={uiRef} _pointerRef={pointerRef} />
+
       <Footer _uiRef={uiRef} _pointerRef={pointerRef} />
       <FooterStyle _uiRef={uiRef} _pointerRef={pointerRef} />
+
       <Right _uiRef={uiRef} _pointerRef={pointerRef} />
       <RightStyle _uiRef={uiRef} _pointerRef={pointerRef} />
+
       <Debug
         _container={containerRef.current}
         _uiRef={uiRef}
         _pointerRef={pointerRef}
         _searchRef={searchRef}
       />
-      <style>
-        {scrollStyle}
-        {modeStyle}
-        {dragStyle}
-        {moveStyle}
-        {zoomStyle}
-      </style>
     </>
   )
 }
