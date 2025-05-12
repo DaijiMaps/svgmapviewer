6e72b35304450f4252fbfeeccad8ef7642b662df
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-12 15:35:57 +0900

    2

--- packages/lib/src/Container.tsx
+++ packages/lib/src/Container.tsx
@@ -4,7 +4,6 @@ import { PointerRef } from './lib/pointer-xstate'
 import {
   useDragStyle,
   useInitStyle,
-  useMapHtmlStyle,
   useModeStyle,
   useMoveStyle,
   useScrollStyle,
@@ -43,7 +42,6 @@ export function ContainerStyle(
       <DragStyle _pointerRef={pointerRef} />
       <MoveStyle _pointerRef={pointerRef} />
       <ZoomStyle _pointerRef={pointerRef} />
-      <MapHtmlStyle _pointerRef={pointerRef} />
     </>
   )
 }
@@ -83,11 +81,5 @@ function MoveStyle(props: Readonly<{ _pointerRef: PointerRef }>) {
 function ZoomStyle(props: Readonly<{ _pointerRef: PointerRef }>) {
   const { _pointerRef: pointerRef } = props
   const style = useZoomStyle(pointerRef)
-  return <style>{style}</style>
-}
-
-function MapHtmlStyle(props: Readonly<{ _pointerRef: PointerRef }>) {
-  const { _pointerRef: pointerRef } = props
-  const style = useMapHtmlStyle(pointerRef)
   return <style>{style}</style>
 }
