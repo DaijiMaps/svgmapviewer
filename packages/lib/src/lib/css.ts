342cb7a4fe241a05493e82e5116b2e3d0992ff91
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-12 15:47:35 +0900

    stricter css matrix detection

--- packages/lib/src/lib/css.ts
+++ packages/lib/src/lib/css.ts
@@ -3,7 +3,13 @@ import { MatrixMatrix as Matrix, matrixToString } from './matrix/prefixed'
 export type CSSMatrix = Readonly<Matrix | Matrix[]>
 
 export function isCssMatrixMatrix(m: CSSMatrix): m is Matrix {
-  return m instanceof Array && m.length === 3
+  return (
+    m instanceof Array &&
+    m.length === 3 &&
+    m[0] instanceof Array &&
+    m[0].length === 2 &&
+    typeof m[0][0] === 'number'
+  )
 }
 
 export function isCssMatrixArray(m: CSSMatrix): m is Matrix[] {
