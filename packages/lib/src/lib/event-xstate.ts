358fb1e0cd40684058510b6d5c9178ffcff992e6
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-16 13:52:55 +0900

    optimize event

--- packages/lib/src/lib/event-xstate.ts
+++ packages/lib/src/lib/event-xstate.ts
@@ -19,6 +19,8 @@ type TimeoutEmit = {
   ev: Event
 }
 
+const EXPIRATION = 2000
+
 export const timeoutMachine = setup({
   types: {
     context: {} as TimeoutContext,
@@ -26,7 +28,7 @@ export const timeoutMachine = setup({
     emitted: {} as TimeoutEmit,
   },
   delays: {
-    EXPIRATION: 2000,
+    EXPIRATION,
   },
 }).createMachine({
   id: 'timeout1',
@@ -63,10 +65,22 @@ export const timeoutMachine = setup({
         },
       },
       on: {
-        TICK: {
-          // update - re-entry
-          target: 'Updating',
-        },
+        TICK: [
+          {
+            guard: ({ context, event }) =>
+              context.ev === null ||
+              // too frequent -> ignore
+              // (hopefully save some CPU time by avoiding timer stop/start too often)
+              event.ev.timeStamp - context.ev.timeStamp < EXPIRATION / 10,
+          },
+          {
+            // update - re-entry
+            actions: assign({
+              ev: ({ event }) => event.ev,
+            }),
+            target: 'Updating',
+          },
+        ],
         STOP: {
           target: 'Stopped',
         },
