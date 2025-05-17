a071e1730329f8b5cef9b39c09e603434cd05f22
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-17 15:54:59 +0900

    wip

--- packages/lib/src/MapHtml.tsx
+++ packages/lib/src/MapHtml.tsx
@@ -1,6 +1,8 @@
 import { configContext, selectMapNames } from '@daijimaps/svgmapviewer'
 import { useSelector } from '@xstate/react'
-import { ReactNode, useContext, useMemo } from 'react'
+import { ReactNode, useContext, useEffect, useMemo } from 'react'
+import { createRoot } from 'react-dom/client'
+import { assign, createActor, emit, setup } from 'xstate'
 import { LayersStyle } from './Layers'
 import { svgMapViewerConfig as cfg } from './lib/config'
 import { fixupCssString } from './lib/css'
@@ -64,17 +66,36 @@ function useNames() {
 }
 
 function MapHtmlContent() {
+  //const names = useNames()
+
   return (
     <>
       <MapHtmlContentSymbols />
       <MapHtmlContentStars />
-      <MapHtmlContentNames />
+      {/*
+      <MapHtmlContentNames _names={names} />
+      */}
+      <div className="poi-names" id={ROOT_ID} />
       <LayersStyle />
     </>
   )
 }
 
 function MapHtmlContentStyle(props: Readonly<MapHtmlProps>) {
+  const names = useNames()
+
+  // eslint-disable-next-line functional/no-expression-statements, functional/no-return-void
+  useEffect(() => {
+    // eslint-disable-next-line functional/no-expression-statements
+    mountMapHtmlContentRoot(props._pointerRef)
+  })
+
+  // eslint-disable-next-line functional/no-expression-statements, functional/no-return-void
+  useEffect(() => {
+    // eslint-disable-next-line functional/no-expression-statements
+    rootActor.send({ type: 'NAMES', names })
+  }, [names])
+
   return (
     <>
       <MapHtmlContentNamesStyle {...props} />
@@ -143,8 +164,8 @@ function MapHtmlContentStars() {
   )
 }
 
-function MapHtmlContentNames() {
-  const names = useNames()
+function MapHtmlContentNames(props: Readonly<{ _names: POI[] }>) {
+  const { _names: names } = props
 
   return (
     <div className="poi-names">
@@ -184,7 +205,7 @@ ${names
   .map(({ id, pos: { x, y }, size }) => {
     const ss = size / s
     const MAX = 500
-    const MIN = 100
+    const MIN = 0
     const opacity = ss > MAX ? 0 : ss < MIN ? 1 : (MAX - ss) / (MAX - MIN)
     return `
 .poi-names-item.osm-id-${id} {
@@ -228,4 +249,135 @@ function RenderName(props: Readonly<{ poi: POI }>) {
       ))}
     </>
   )
+}
+
+//// shadow DOM actor
+
+const ROOT_ID = 'map-html-content-root'
+
+type RootEvent =
+  | {
+      type: 'MOUNT'
+      ref: PointerRef
+    }
+  | {
+      type: 'NAMES'
+      names: POI[]
+    }
+  | {
+      type: 'RENDER'
+    }
+type RootEmit = {
+  type: 'RENDER'
+  ref: null | PointerRef
+  names: POI[]
+}
+interface RootContext {
+  ref: null | PointerRef
+  names: POI[]
+}
+
+const rootLogic = setup({
+  types: {
+    events: {} as RootEvent,
+    emitted: {} as RootEmit,
+    context: {} as RootContext,
+  },
+}).createMachine({
+  id: 'map-html-names-root',
+  context: { ref: null, names: [] },
+  on: {
+    MOUNT: {
+      actions: assign({
+        ref: ({ event }) => event.ref,
+      }),
+    },
+    NAMES: [
+      {
+        guard: ({ context }) => context.ref === null,
+      },
+      {
+        guard: ({ context }) => context.ref !== null,
+        actions: [
+          assign({
+            names: ({ event }) => event.names,
+          }),
+          emit(({ context, event }) => ({
+            type: 'RENDER',
+            ref: context.ref,
+            names: event.names,
+          })),
+        ],
+      },
+    ],
+    RENDER: {
+      actions: emit(({ context }) => ({
+        type: 'RENDER',
+        ref: context.ref,
+        names: context.names,
+      })),
+    },
+  },
+})
+
+const rootActor = createActor(rootLogic)
+
+// eslint-disable-next-line functional/no-expression-statements
+rootActor.on('RENDER', ({ ref, names }) => {
+  if (ref === null) {
+    return
+  }
+  return renderShadowRoot(
+    ROOT_ID,
+    <>
+      <style>
+        {`
+.poi-names-item {
+  position: absolute;
+  padding: 0.5em;
+  background-color: rgba(255, 255, 255, 0.375);
+  text-align: center;
+  border-radius: 5em;
+}
+.poi-names-item > p {
+  margin: 0;
+}
+`}
+      </style>
+      <MapHtmlContentNames _names={names} />
+      <MapHtmlContentStyle _pointerRef={ref} />
+    </>
+  )
+})
+
+// eslint-disable-next-line functional/no-expression-statements
+rootActor.start()
+
+// eslint-disable-next-line functional/no-return-void
+export function mountMapHtmlContentRoot(ref: Readonly<PointerRef>) {
+  const root = document.querySelector(`#${ROOT_ID}`)
+  if (root === null) {
+    return
+  }
+  if (root.shadowRoot !== null) {
+    return
+  }
+  // eslint-disable-next-line functional/no-expression-statements
+  rootActor.send({ type: 'MOUNT', ref })
+}
+
+//// shadow DOM render
+
+function renderShadowRoot(id: string, children: Readonly<ReactNode>): boolean {
+  const root = document.querySelector(`#${id}`)
+  if (root === null) {
+    return false
+  }
+  if (root.shadowRoot !== null) {
+    return true
+  }
+  const shadowRoot = root.attachShadow({ mode: 'open' })
+  // eslint-disable-next-line functional/no-expression-statements
+  createRoot(shadowRoot).render(children)
+  return true
 }
