89538f8f409fb03a7436d24170d6c03728df66ad
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-21 01:23:55 +0900

    actually render detail now

--- packages/lib/src/Ui.tsx
+++ packages/lib/src/Ui.tsx
@@ -1,10 +1,8 @@
-import { useSelector } from '@xstate/react'
 import { Debug } from './Debug'
 import { Detail } from './Detail'
 import { Footer } from './Footer'
 import { Header } from './Header'
 import { searchRef } from './lib/search'
-import { selectDetail, uiActor } from './lib/ui-xstate'
 import { Right } from './Right'
 import { Shadow } from './Shadow'
 
@@ -12,21 +10,10 @@ import { Shadow } from './Shadow'
 // XXX receive animationend here (.ui)
 
 export function Ui() {
-  // XXX
-  // XXX
-  // XXX
-  // XXX - hide Detail as it uses layout
-  // XXX - ideally detail/balloon draws shapes only via css
-  // XXX - then Detail can be unconditionally rendered
-  // XXX
-  // XXX
-  // XXX
-  const detail = useSelector(uiActor, selectDetail)
-
   return (
     <div id="ui">
       <Shadow />
-      {detail !== null && <Detail />}
+      <Detail />
       <Header />
       <Footer />
       <Right />
