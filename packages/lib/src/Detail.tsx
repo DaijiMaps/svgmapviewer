8b493858ee6f8eba15c93aa88e608ac0e90b42ea
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-23 10:12:25 +0900

    don't re-render Detail after close

--- packages/lib/src/Detail.tsx
+++ packages/lib/src/Detail.tsx
@@ -3,17 +3,10 @@ import { Balloon, BalloonStyle } from './Balloon'
 import './Detail.css'
 import { svgMapViewerConfig as cfg } from './lib/config'
 import { diag } from './lib/diag'
-import { openCloseIsVisible } from './lib/openclose'
-import {
-  isDetailEmpty,
-  selectDetail,
-  selectOpenCloseDetail,
-  uiActor,
-} from './lib/ui-xstate'
+import { isDetailEmpty, selectDetail, uiActor } from './lib/ui-xstate'
 
 export function Detail() {
   const detail = useSelector(uiActor, selectDetail)
-  const oc = useSelector(uiActor, selectOpenCloseDetail)
 
   const p = detail.p
   const layout = detail.layout
@@ -24,13 +17,7 @@ export function Detail() {
   const H = layout.container.height
 
   return (
-    <div
-      className="detail-balloon"
-      style={{
-        // XXX overidden when detail/balloon is open
-        display: detail !== null && openCloseIsVisible(oc) ? 'initial' : 'none',
-      }}
-    >
+    <div className="detail-balloon">
       <Balloon _detail={detail} _p={p} _dir={dir} _W={W} _H={H}>
         <BalloonStyle _detail={detail} _p={p} _dir={dir} _W={W} _H={H} />
       </Balloon>
