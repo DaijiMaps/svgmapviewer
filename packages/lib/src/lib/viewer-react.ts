018ed9180537865c61dca7b893c0d80e0a4f295a
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-29 22:30:46 +0900

    refactor

--- packages/lib/src/lib/viewer-react.ts
+++ packages/lib/src/lib/viewer-react.ts
@@ -2,31 +2,35 @@ import React from 'react'
 import { setCurrentScroll } from './scroll'
 import { clickeventmask, viewerSend, viewerSendEvent } from './viewer-xstate'
 
-export const sendClick = (ev: React.MouseEvent<HTMLDivElement>): void => {
+export function sendClick(ev: React.MouseEvent<HTMLDivElement>): void {
   if (clickeventmask) {
     return
   }
   viewerSendEvent({ type: 'CLICK', ev })
 }
-export const sendContextMenu = (ev: React.MouseEvent<HTMLDivElement>): void =>
+export function sendContextMenu(ev: React.MouseEvent<HTMLDivElement>): void {
   viewerSendEvent({ type: 'CONTEXTMENU', ev })
-export const sendWheel = (ev: React.WheelEvent<HTMLDivElement>): void => {
+}
+export function sendWheel(ev: React.WheelEvent<HTMLDivElement>): void {
   viewerSendEvent({ type: 'WHEEL', ev })
 }
-export const sendScroll = (ev: React.UIEvent<HTMLDivElement, Event>): void => {
+export function sendScroll(ev: React.UIEvent<HTMLDivElement, Event>): void {
   if (ev !== null) {
     setCurrentScroll(ev)
   }
 }
-export const sendAnimationEnd = (
+export function sendAnimationEnd(
   ev: React.AnimationEvent<HTMLDivElement>
-): void =>
+): void {
   viewerSendEvent({
     type: 'ANIMATION.END',
     ev,
   })
+}
 
-export const keyDown = (ev: KeyboardEvent): void =>
+export function keyDown(ev: KeyboardEvent): void {
   viewerSend({ type: 'KEY.DOWN', ev })
-export const keyUp = (ev: KeyboardEvent): void =>
+}
+export function keyUp(ev: KeyboardEvent): void {
   viewerSend({ type: 'KEY.UP', ev })
+}
