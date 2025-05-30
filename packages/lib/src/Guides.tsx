a6070701763e2af0f43f70ee860d73df6aea172e
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-30 19:43:50 +0900

    wip

--- packages/lib/src/Guides.tsx
+++ packages/lib/src/Guides.tsx
@@ -53,6 +53,20 @@ function Measure(): ReactNode {
     <g>
       <text id="longitude">E 135.123456</text>
       <text id="latitude">N 35.123456</text>
+      <g className="distance-x">
+        {INDEXES.map((i, idx) => (
+          <text key={idx} id={`distance-x-${i}`}>
+            {(i + 1) * 10 + `m`}
+          </text>
+        ))}
+      </g>
+      <g className="distance-y">
+        {INDEXES.map((i, idx) => (
+          <text key={idx} id={`distance-y-${i}`}>
+            {(i + 1) * 10 + `m`}
+          </text>
+        ))}
+      </g>
       <path id="measure" stroke="black" strokeWidth="0.15px" fill="none" d="" />
     </g>
   )
@@ -66,14 +80,10 @@ export function MeasureStyle(): ReactNode {
 
   const horizontal = `M0,${height / 2} h${width}`
   const vertical = `M${width / 2},0 v${height}`
-  const rings = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ]
-    .map((i) => {
-      const r = 100 * (i + 1)
-      return `M${width / 2},${height / 2} m-${r},0 a${r},${r} 0,1,0 ${r * 2},0 a${r},${r} 0,1,0 -${r * 2},0`
-    })
-    .join(' ')
+  const rings = INDEXES.map((i) => {
+    const r = 100 * (i + 1)
+    return `M${width / 2},${height / 2} m-${r},0 a${r},${r} 0,1,0 ${r * 2},0 a${r},${r} 0,1,0 -${r * 2},0`
+  }).join(' ')
   // XXX no newlines allowed
   const d = `${horizontal} ${vertical} ${rings}`
   const pathStyle = `
@@ -113,18 +123,44 @@ export function MeasureStyle(): ReactNode {
 
   const longitudeStyle = `
 #longitude {
-  transform: translate(${width / 2 + 1}px, ${height / 2 + 8}px) scale(0.5);
+  transform: translate(${width / 2 + 2}px, ${height / 2 - 2}px) scale(0.5);
   font-size: medium;
   font-weight: lighter;
+  text-anchor: start;
 }
 `
   const latitudeStyle = `
 #latitude {
-  transform: translate(${width / 2 + 1}px, ${height / 2 - 2}px) scale(0.5);
+  transform: translate(${width / 2 - 2}px, ${height / 2 - 2}px) scale(0.5);
   font-size: medium;
   font-weight: lighter;
+  text-anchor: end;
 }
 `
+
+  const distanceXStyle = INDEXES.map((i) => {
+    const r = 100 * (i + 1)
+    return `
+#distance-x-${i} {
+  transform: translate(${width / 2 + 2 + r}px, ${height / 2 + 8}px) scale(0.5);
+  font-size: medium;
+  font-weight: lighter;
+  text-anchor: start;
+}
+`
+  })
+
+  const distanceYStyle = INDEXES.map((i) => {
+    const r = 100 * (i + 1)
+    return `
+#distance-y-${i} {
+  transform: translate(${width / 2 + 2}px, ${height / 2 + 8 + r}px) scale(0.5);
+  font-size: medium;
+  font-weight: lighter;
+  text-anchor: start;
+}
+`
+  })
 
   return (
     <>
@@ -132,6 +168,18 @@ export function MeasureStyle(): ReactNode {
       {animationStyle}
       {longitudeStyle}
       {latitudeStyle}
+      {distanceXStyle}
+      {distanceYStyle}
     </>
   )
 }
+
+// XXX
+// XXX
+// XXX
+const INDEXES = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+]
+// XXX
+// XXX
+// XXX
