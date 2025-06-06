5addf1361789e85ec14691983918d0869052b468
Masao Uebayashi <uebayasi@tombiinc.com>
2025-06-06 14:22:28 +0900

    set explicit width/height of inner svg

--- packages/lib/src/MapSvg.tsx
+++ packages/lib/src/MapSvg.tsx
@@ -2,6 +2,7 @@
 import { type ReactNode } from 'react'
 import { RenderMapCommon } from './lib/carto'
 import { useMapSvgRendered } from './lib/map-svg-react'
+import { useLayout } from './lib/style-xstate'
 import './MapSvg.css'
 import { MapSvgStyle } from './MapSvgStyle'
 
@@ -9,10 +10,17 @@ export function MapSvg(): ReactNode {
   // eslint-disable-next-line functional/no-expression-statements
   useMapSvgRendered()
 
+  const { scroll } = useLayout()
+
   // viewBox will be updated by syncViewBox()
   return (
     <>
-      <svg id="map-svg-svg" viewBox="0 0 1 1" width="100%" height="100%">
+      <svg
+        id="map-svg-svg"
+        viewBox="0 0 1 1"
+        width={scroll.width}
+        height={scroll.height}
+      >
         <RenderMapCommon />
       </svg>
       <MapSvgStyle />
