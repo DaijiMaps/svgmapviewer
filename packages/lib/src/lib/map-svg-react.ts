fc3b88bf67dd88ef2cd7d9c2c1efd945ea2c2124
Masao Uebayashi <uebayasi@tombiinc.com>
2025-06-07 11:35:51 +0900

    refactor

--- packages/lib/src/lib/map-svg-react.ts
+++ packages/lib/src/lib/map-svg-react.ts
@@ -1,10 +1,10 @@
 import { useEffect } from 'react'
 import { viewerSend } from './viewer-xstate'
 
-export const MAP_SVG_ROOT_ID = 'map-svg-content-root'
+export const MAP_SVG_ROOT_ID = 'map-svg-root'
 export const MAP_SVG_CONTENT_ID = 'map-svg-svg'
-export const MAP_SVG_SYMBOLS_ROOT_ID = 'map-svg-symbols-content-root'
-export const MAP_SVG_SYMBOLS_CONTENT_ID = 'map-svg-svg-symbols'
+export const MAP_SVG_SYMBOLS_ROOT_ID = 'map-svg-symbols-root'
+export const MAP_SVG_SYMBOLS_CONTENT_ID = 'map-svg-symbols-svg'
 
 let mapSvgRendered = false
 let mapSvgSymbolsRendered = false
