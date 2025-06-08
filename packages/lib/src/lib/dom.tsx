b6b3d52defdaf98c9c1732a0431c07a0eed29c10
Masao Uebayashi <uebayasi@tombiinc.com>
2025-06-08 12:29:08 +0900

    refactor

--- packages/lib/src/lib/dom.tsx
+++ packages/lib/src/lib/dom.tsx
@@ -1,18 +1,6 @@
 import { type ReactNode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-// eslint-disable-next-line functional/no-return-void
-export function mountShadowRoot(id: string): void {
-  const root = document.querySelector(`#${id}`)
-  if (root === null || root.shadowRoot === null) {
-    return
-  }
-
-  //rootActor.send({ type: 'MOUNT', ref })
-}
-
-//// shadow DOM render
-
 export function renderShadowRoot(
   id: string,
   children: Readonly<ReactNode>
