41ccd37f8d745311f7f1a308bd9a0ba8cdd0c2a3
Masao Uebayashi <uebayasi@tombiinc.com>
2025-07-19 20:25:56 +0900

    refactor

--- packages/lib/src/lib/map/Floor.tsx
+++ packages/lib/src/lib/map/Floor.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable functional/functional-parameters */
-import type { ReactNode } from 'react'
+import { Fragment, type ReactNode } from 'react'
 import { svgMapViewerConfig } from '../../config'
 import { useLayout2 } from '../../style-xstate'
+import type { FloorsConfig } from '../../types'
 import { useFloors } from '../viewer/floors-xstate'
 
 export function RenderFloors(): ReactNode {
   const { viewBox, width, height } = useLayout2()
-  const { fidxToOnAnimationEnd } = useFloors()
 
-  const { origViewBox, floorsConfig } = svgMapViewerConfig
+  const { floorsConfig } = svgMapViewerConfig
 
   if (floorsConfig === undefined) {
     return <></>
@@ -17,18 +17,44 @@ export function RenderFloors(): ReactNode {
   return (
     <svg viewBox={viewBox} width={width} height={height}>
       {floorsConfig.floors.map(({ href }, idx) => (
-        <image
-          key={idx}
-          className={`floor fidx-${idx}`}
-          href={href}
-          onAnimationEnd={fidxToOnAnimationEnd(idx)}
-          x={origViewBox.x}
-          y={origViewBox.y}
-          width={origViewBox.width}
-          height={origViewBox.height}
-        />
+        <Fragment key={idx}>
+          <RenderFloorImage
+            _floorsConfig={floorsConfig}
+            _href={href}
+            _idx={idx}
+          />
+        </Fragment>
       ))}
     </svg>
+  )
+}
+
+function RenderFloorImage(
+  props: Readonly<{ _floorsConfig: FloorsConfig; _href: string; _idx: number }>
+): ReactNode {
+  const { _href: href, _idx: idx } = props
+  const { fidxToOnAnimationEnd } = useFloors()
+  const {
+    origViewBox: { x, y, width, height },
+  } = svgMapViewerConfig
+
+  const hw = width / 2
+  const hh = height / 2
+
+  return (
+    <image
+      className={`floor fidx-${idx}`}
+      href={href}
+      onAnimationEnd={fidxToOnAnimationEnd(idx)}
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      style={{
+        transformOrigin: 'left top',
+        transform: `translate(${hw}px, ${hh}px) rotate(90deg) translate(${-hw}px, ${-hh}px)`,
+      }}
+    />
   )
 }
 
