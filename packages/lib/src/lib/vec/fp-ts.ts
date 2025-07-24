c1aebc077096bcaa99d5e5fe2e5e594fc2e851dd
Masao Uebayashi <uebayasi@tombiinc.com>
2025-07-24 13:50:38 +0900

    prefer function

--- packages/lib/src/lib/vec/fp-ts.ts
+++ packages/lib/src/lib/vec/fp-ts.ts
@@ -1,7 +1,8 @@
 import { number, ord as ord_, ordering } from 'fp-ts'
 import { type Vec } from './main'
 
-export const compare = (a: Vec, b: Vec): ordering.Ordering =>
-  number.Ord.compare(a.x, b.x) || number.Ord.compare(a.y, b.y)
+export function compare(a: Vec, b: Vec): ordering.Ordering {
+  return number.Ord.compare(a.x, b.x) || number.Ord.compare(a.y, b.y)
+}
 
 export const ord: ord_.Ord<Vec> = ord_.fromCompare(compare)
