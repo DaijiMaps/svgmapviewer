201ce3032b8fde19bd463df8b167cc70e5917e87
Masao Uebayashi <uebayasi@tombiinc.com>
2025-08-13 00:25:34 +0900

    rslib

--- packages/lib/rslib.config.ts
+++ packages/lib/rslib.config.ts
@@ -1,4 +1,3 @@
-//import { pluginReact } from '@rsbuild/plugin-react'
 import { defineConfig } from '@rslib/core'
 
 export default defineConfig({
@@ -6,15 +5,24 @@ export default defineConfig({
     {
       format: 'esm',
       syntax: 'esnext',
-      dts: { bundle: true },
+      dts: { bundle: false, distPath: './dist' },
     },
   ],
   source: {
     entry: {
-      index: 'src/index.ts',
-      'search-worker': 'src/lib/search/search-worker.ts',
+      'index': 'src/index.ts',
+      'lib/box/prefixed/index': 'src/lib/box/prefixed/index.ts',
+      'lib/carto/index': 'src/lib/carto/index.ts',
+      'lib/carto/objects/index': 'src/lib/carto/objects/index.ts',
+      'lib/carto/symbols/index': 'src/lib/carto/symbols/index.ts',
+      'lib/geo/index': 'src/lib/geo/index.ts',
+      'lib/map/index': 'src/lib/map/index.ts',
+      'lib/matrix/prefixed/index': 'src/lib/matrix/prefixed/index.ts',
+      'lib/search/index': 'src/lib/search/index.ts',
+      'lib/search/search-worker': 'src/lib/search/search-worker.ts',
+      'lib/tuple/index': 'src/lib/box/index.ts',
+      'lib/vec/prefixed/index': 'src/lib/box/prefixed/index.ts',
     },
     tsconfigPath: './tsconfig.app.json',
   },
-  //plugins: [pluginReact()],
 })
