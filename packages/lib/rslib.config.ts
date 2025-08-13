5846aef1a78a1ad0816fb6af6daf9ed4a4b3fd47
Masao Uebayashi <uebayasi@tombiinc.com>
2025-08-14 01:15:52 +0900

    rslib plugin-react

--- packages/lib/rslib.config.ts
+++ packages/lib/rslib.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from '@rslib/core'
+import { pluginReact } from '@rsbuild/plugin-react'
 
 export default defineConfig({
+  plugins: [pluginReact()],
   lib: [
     {
       format: 'esm',
