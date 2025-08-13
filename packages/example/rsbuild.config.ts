d1ddccc0b365b9ffff42d4e597261a92a31e7d53
Masao Uebayashi <uebayasi@tombiinc.com>
2025-08-13 15:14:10 +0900

    rsbuild example

--- packages/example/rsbuild.config.ts
+++ packages/example/rsbuild.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
 
-// XXX rsbuild is used only for dev
+// XXX rsbuild is used
 // XXX until vite fixes handling of libs with separate worker .js files
 
 export default defineConfig({
+  plugins: [pluginReact()],
   server: {
     base: '',
+    port: 5173,
   },
   html: {
     template: './template.html',
