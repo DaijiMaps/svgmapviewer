9d46ac2f9449ff73dcc291625906f5740976bf9b
Masao Uebayashi <uebayasi@tombiinc.com>
2025-12-14 18:16:04 +0900

    refactor scripts

---
+++ packages/example/src/assets/split-layers.sh
@@ -0,0 +1,4 @@
+inkscape="inkscape floors.inkscape.svg --export-id-only --export-type=svg --export-plain-svg"
+
+$inkscape --export-id="layer1" --export-filename=1f.svg
+$inkscape --export-id="layer2" --export-filename=2f.svg
