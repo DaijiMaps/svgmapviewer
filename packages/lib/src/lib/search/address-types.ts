594864d7a97802468f404791961f3fea33dba071
Masao Uebayashi <uebayasi@tombiinc.com>
2025-12-17 10:24:37 +0900

    refactor

--- packages/lib/src/lib/search/address-types.ts
+++ packages/lib/src/lib/search/address-types.ts
@@ -4,8 +4,6 @@ import { type VecVec } from '../vec/prefixed'
 export type Address = string
 export type Idx = string
 
-export type FlatbushIndexes = Record<Idx, AddressEntry>
-
 export interface AddressEntry {
   address: Address
   coord: VecVec
@@ -13,12 +11,16 @@ export interface AddressEntry {
 }
 export type AddressEntries = readonly AddressEntry[]
 
-export interface SearchContext {
-  fb: Flatbush
-  idxs: FlatbushIndexes
-}
-
 export interface SearchAddressRes {
   address: Address
   coord: VecVec
+}
+
+////
+
+export type FlatbushIndexes = Record<Idx, AddressEntry>
+
+export interface SearchContext {
+  fb: Flatbush
+  idxs: FlatbushIndexes
 }
