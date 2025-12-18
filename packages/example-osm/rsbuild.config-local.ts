76bfe74f00eb6ac5d203e3413a0f2e3943855fb3
Masao Uebayashi <uebayasi@tombiinc.com>
2025-12-19 02:21:54 +0900

    chore: put back rsbuild local config

---
+++ packages/example-osm/rsbuild.config-local.ts
@@ -0,0 +1,57 @@
+import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
+import path from 'node:path'
+
+const APP = '../app-osm/src'
+const LIB = '../lib/src/lib'
+
+export default defineConfig({
+  plugins: [pluginReact()],
+  server: {
+    base: '',
+    port: 5173,
+  },
+  html: {
+    template: './index.html',
+  },
+  source: {
+    entry: {
+      index: './src/main.ts',
+    },
+  },
+  resolve: {
+    alias: {
+      'svgmapviewer-app-osm': path.resolve(__dirname, `${APP}/index.ts`),
+      'svgmapviewer/box': path.resolve(
+        __dirname,
+        `${LIB}/box/prefixed/index.ts`
+      ),
+      'svgmapviewer/carto-objects': path.resolve(
+        __dirname,
+        `${LIB}/carto/objects/index.ts`
+      ),
+      'svgmapviewer/carto-symbols': path.resolve(
+        __dirname,
+        `${LIB}/carto/symbols/index.ts`
+      ),
+      'svgmapviewer/carto': path.resolve(__dirname, `${LIB}/carto/index.ts`),
+      'svgmapviewer/geo': path.resolve(__dirname, `${LIB}/geo/index.ts`),
+      'svgmapviewer/map': path.resolve(__dirname, `${LIB}/map/index.ts`),
+      'svgmapviewer/map-floors': path.resolve(
+        __dirname,
+        `${LIB}/map/floors/index.ts`
+      ),
+      'svgmapviewer/matrix': path.resolve(
+        __dirname,
+        `${LIB}/matrix/prefixed/index.ts`
+      ),
+      'svgmapviewer/search': path.resolve(__dirname, `${LIB}/search/index.ts`),
+      'svgmapviewer/tuple': path.resolve(__dirname, `${LIB}/tuple/index.ts`),
+      'svgmapviewer/vec': path.resolve(
+        __dirname,
+        `${LIB}/vec/prefixed/index.ts`
+      ),
+      svgmapviewer: path.resolve(__dirname, `${LIB}/../index.ts`),
+    },
+  },
+})
