44f138b54ceddb41387b24a486687aeeb429ce74
Masao Uebayashi <uebayasi@tombiinc.com>
2025-12-19 11:50:45 +0900

    chore

--- packages/example-osm/rsbuild.config-local.ts
+++ packages/example-osm/rsbuild.config-local.ts
@@ -3,7 +3,12 @@ import { pluginReact } from '@rsbuild/plugin-react'
 import path from 'node:path'
 
 const APP = '../app-osm/src'
-const LIB = '../lib/src/lib'
+const LIB = '../lib/src'
+
+const resolve = (dir: string, subdir: string): string =>
+  path.resolve(__dirname, `${dir}/${subdir}`)
+const app = (subdir: string): string => resolve(APP, subdir)
+const lib = (subdir: string): string => resolve(LIB, subdir)
 
 export default defineConfig({
   plugins: [pluginReact()],
@@ -21,37 +26,19 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      'svgmapviewer-app-osm': path.resolve(__dirname, `${APP}/index.ts`),
-      'svgmapviewer/box': path.resolve(
-        __dirname,
-        `${LIB}/box/prefixed/index.ts`
-      ),
-      'svgmapviewer/carto-objects': path.resolve(
-        __dirname,
-        `${LIB}/carto/objects/index.ts`
-      ),
-      'svgmapviewer/carto-symbols': path.resolve(
-        __dirname,
-        `${LIB}/carto/symbols/index.ts`
-      ),
-      'svgmapviewer/carto': path.resolve(__dirname, `${LIB}/carto/index.ts`),
-      'svgmapviewer/geo': path.resolve(__dirname, `${LIB}/geo/index.ts`),
-      'svgmapviewer/map': path.resolve(__dirname, `${LIB}/map/index.ts`),
-      'svgmapviewer/map-floors': path.resolve(
-        __dirname,
-        `${LIB}/map/floors/index.ts`
-      ),
-      'svgmapviewer/matrix': path.resolve(
-        __dirname,
-        `${LIB}/matrix/prefixed/index.ts`
-      ),
-      'svgmapviewer/search': path.resolve(__dirname, `${LIB}/search/index.ts`),
-      'svgmapviewer/tuple': path.resolve(__dirname, `${LIB}/tuple/index.ts`),
-      'svgmapviewer/vec': path.resolve(
-        __dirname,
-        `${LIB}/vec/prefixed/index.ts`
-      ),
-      svgmapviewer: path.resolve(__dirname, `${LIB}/../index.ts`),
+      'svgmapviewer-app-osm': app('index.ts'),
+      'svgmapviewer/box': lib('lib/box/prefixed/index.ts'),
+      'svgmapviewer/carto-objects': lib('lib/carto/objects/index.ts'),
+      'svgmapviewer/carto-symbols': lib('lib/carto/symbols/index.ts'),
+      'svgmapviewer/carto': lib('lib/carto/index.ts'),
+      'svgmapviewer/geo': lib('lib/geo/index.ts'),
+      'svgmapviewer/map': lib('lib/map/index.ts'),
+      'svgmapviewer/map-floors': lib('lib/map/floors/index.ts'),
+      'svgmapviewer/matrix': lib('lib/matrix/prefixed/index.ts'),
+      'svgmapviewer/search': lib('lib/search/index.ts'),
+      'svgmapviewer/tuple': lib('lib/tuple/index.ts'),
+      'svgmapviewer/vec': lib('lib/vec/prefixed/index.ts'),
+      svgmapviewer: lib('index.ts'),
     },
   },
 })
