481adde8dd3a58efc7422aefe5d91d3ba24f9d3f
Masao Uebayashi <uebayasi@tombiinc.com>
2025-12-21 00:43:36 +0900

    refactor

--- packages/lib/src/lib/event-action.ts
+++ packages/lib/src/lib/event-action.ts
@@ -7,31 +7,31 @@ import type { ActionCbs } from './event-action-types'
 export const actionCbs: ActionCbs = {
   zoomIn: new Set(),
   zoomOut: new Set(),
+  rotate: new Set(),
   reset: new Set(),
   recenter: new Set(),
-  rotate: new Set(),
-  position: new Set(),
   fullscreen: new Set(),
+  position: new Set(),
 }
 
+export function notifyActionZoomIn(): void {
+  notifyCbs0(actionCbs.zoomIn)
+}
+export function notifyActionZoomOut(): void {
+  notifyCbs0(actionCbs.zoomOut)
+}
+export function notifyActionRotate(): void {
+  notifyCbs0(actionCbs.rotate)
+}
+export function notifyActionReset(): void {
+  notifyCbs0(actionCbs.reset)
+}
+export function notifyActionRecenter(): void {
+  notifyCbs0(actionCbs.recenter)
+}
 export function notifyActionFullscreen(): void {
   notifyCbs0(actionCbs.fullscreen)
 }
 export function notifyActionPosition(): void {
   notifyCbs0(actionCbs.position)
-}
-export function notifyActionRecenter(): void {
-  notifyCbs0(actionCbs.recenter)
-}
-export function notifyActionReset(): void {
-  notifyCbs0(actionCbs.reset)
-}
-export function notifyActionRotate(): void {
-  notifyCbs0(actionCbs.rotate)
-}
-export function notifyActionZoomOut(): void {
-  notifyCbs0(actionCbs.zoomOut)
-}
-export function notifyActionZoomIn(): void {
-  notifyCbs0(actionCbs.zoomIn)
 }
