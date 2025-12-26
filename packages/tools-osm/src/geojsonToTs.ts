f1c86aacde9b391bda3297f69458f5467a356d73
Masao Uebayashi <uebayasi@tombiinc.com>
2025-12-26 11:09:56 +0900

    geojsonToTs

--- packages/tools-osm/src/geojsonToTs.ts
+++ packages/tools-osm/src/geojsonToTs.ts
@@ -2,30 +2,47 @@
 import { FileSystem } from '@effect/platform'
 import { NodeContext, NodeRuntime } from '@effect/platform-node'
 import { Doc } from '@effect/printer'
-import { Effect } from 'effect'
+import { Effect, Record } from 'effect'
 import { decodeGeoJSON } from './lib/geojson'
-import { printGeoJsonAsTs } from './lib/print'
+import { printAllTs, printGeoJsonAsTs } from './lib/print'
 
-// XXX - effect Schema
 // XXX - split other_tags
+// XXX - write all.ts
 
 const NodeContextLive = NodeContext.layer
 
+const convName = (_type: _Type) =>
+  Effect.gen(function* () {
+    const fs = yield* FileSystem.FileSystem
+    const jsonStr = yield* fs.readFileString(`./${_type}.json`)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const jsonObj = JSON.parse(jsonStr)
+    const geojson = decodeGeoJSON(jsonObj)
+    const doc = printGeoJsonAsTs(_type, _types[_type], geojson)
+    const ts = Doc.render(doc, { style: 'pretty' })
+    yield* fs.writeFileString(`./${_type}.ts`, ts)
+  })
+
+// eslint-disable-next-line functional/functional-parameters
+const saveAllTs = () =>
+  Effect.gen(function* () {
+    const fs = yield* FileSystem.FileSystem
+    const doc = printAllTs()
+    const ts = Doc.render(doc, { style: 'pretty' })
+    yield* fs.writeFileString(`./all.ts`, ts)
+  })
+
 const program = Effect.gen(function* () {
-  const fs = yield* FileSystem.FileSystem
-  const jsonStr = yield* fs.readFileString('./areas.json')
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  const jsonObj = JSON.parse(jsonStr)
-  const geojson = decodeGeoJSON(jsonObj)
-  const doc = printGeoJsonAsTs('areas', 'MultiPolygonGeoJSON', geojson)
-  console.log(Doc.render(doc, { style: 'pretty' }))
-  return geojson
+  yield* Effect.all(Record.keys(_types).map(convName)).pipe(
+    Effect.zip(saveAllTs())
+  )
 }).pipe(Effect.provide(NodeContextLive))
 
 NodeRuntime.runMain(program)
 
-/*
-const names = {
+////
+
+const _types = {
   areas: 'MultiPolygonGeoJSON',
   internals: 'MultiPolygonGeoJSON',
   origin: 'PointGeoJSON',
@@ -38,5 +55,21 @@ const names = {
   'map-multipolygons': 'OsmMultiPolygonGeoJSON',
 }
 
-type Names = keyof typeof typeNames
+type _Type = keyof typeof _types
+
+/*
+const _names = {
+  areas: 'areas',
+  internals: 'internals',
+  origin: 'origin',
+  measures: 'measures',
+  viewbox: 'viewbox',
+
+  'map-points': 'points',
+  'map-lines': 'lines',
+  'map-multilinestrings': 'multilinestrings',
+  'map-multipolygons': 'multipolygons',
+}
+
+type _Name = keyof typeof _names
 */
