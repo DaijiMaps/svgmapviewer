5d1f234d9fd2ced506a5ed435e64e5e3a62130d5
Masao Uebayashi <uebayasi@tombiinc.com>
2025-12-26 12:30:21 +0900

    tools-osm

--- packages/tools-osm/src/geojsonToTs.ts
+++ packages/tools-osm/src/geojsonToTs.ts
@@ -1,75 +1,14 @@
 /* eslint-disable functional/no-expression-statements */
-import { FileSystem } from '@effect/platform'
 import { NodeContext, NodeRuntime } from '@effect/platform-node'
-import { Doc } from '@effect/printer'
-import { Effect, Record } from 'effect'
-import { decodeGeoJSON } from './lib/geojson'
-import { printAllTs, printGeoJsonAsTs } from './lib/print'
+import { Effect } from 'effect'
+import { convNames, saveAllTs } from './lib/print'
 
 // XXX - split other_tags
-// XXX - write all.ts
 
 const NodeContextLive = NodeContext.layer
 
-const convName = (_type: _Type) =>
-  Effect.gen(function* () {
-    const fs = yield* FileSystem.FileSystem
-    const jsonStr = yield* fs.readFileString(`./${_type}.json`)
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const jsonObj = JSON.parse(jsonStr)
-    const geojson = decodeGeoJSON(jsonObj)
-    const doc = printGeoJsonAsTs(_type, _types[_type], geojson)
-    const ts = Doc.render(doc, { style: 'pretty' })
-    yield* fs.writeFileString(`./${_type}.ts`, ts)
-  })
-
-// eslint-disable-next-line functional/functional-parameters
-const saveAllTs = () =>
-  Effect.gen(function* () {
-    const fs = yield* FileSystem.FileSystem
-    const doc = printAllTs()
-    const ts = Doc.render(doc, { style: 'pretty' })
-    yield* fs.writeFileString(`./all.ts`, ts)
-  })
-
 const program = Effect.gen(function* () {
-  yield* Effect.all(Record.keys(_types).map(convName)).pipe(
-    Effect.zip(saveAllTs())
-  )
+  return yield* convNames().pipe(Effect.zip(saveAllTs()))
 }).pipe(Effect.provide(NodeContextLive))
 
 NodeRuntime.runMain(program)
-
-////
-
-const _types = {
-  areas: 'MultiPolygonGeoJSON',
-  internals: 'MultiPolygonGeoJSON',
-  origin: 'PointGeoJSON',
-  measures: 'LineGeoJSON<MeasureProperties>',
-  viewbox: 'LineGeoJSON',
-
-  'map-points': 'OsmPointGeoJSON',
-  'map-lines': 'OsmLineGeoJSON',
-  'map-multilinestrings': 'OsmMultiLineStringGeoJSON',
-  'map-multipolygons': 'OsmMultiPolygonGeoJSON',
-}
-
-type _Type = keyof typeof _types
-
-/*
-const _names = {
-  areas: 'areas',
-  internals: 'internals',
-  origin: 'origin',
-  measures: 'measures',
-  viewbox: 'viewbox',
-
-  'map-points': 'points',
-  'map-lines': 'lines',
-  'map-multilinestrings': 'multilinestrings',
-  'map-multipolygons': 'multipolygons',
-}
-
-type _Name = keyof typeof _names
-*/
--- packages/tools-osm/src/lib/print.ts
+++ packages/tools-osm/src/lib/print.ts
@@ -1,4 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { FileSystem } from '@effect/platform'
+import type { PlatformError } from '@effect/platform/Error'
 import { Doc } from '@effect/printer'
+import { Effect, Record } from 'effect'
+import { decodeGeoJSON } from './geojson'
 import { printGeoJSON } from './geojson-print'
 import type { _GeoJSON } from './geojson-types'
 
@@ -19,27 +24,82 @@ export function printGeoJsonAsTs(
 
 // eslint-disable-next-line functional/functional-parameters
 export function printAllTs(): Doc.Doc<never> {
-  return Doc.text(`
-import areas from './areas'
-import internals from './internals'
-import lines from './map-lines'
-import multilinestrings from './map-multilinestrings'
-import multipolygons from './map-multipolygons'
-import points from './map-points'
-import measures from './measures'
-import origin from './origin'
-import viewbox from './viewbox'
-
-export const mapData = {
-  areas,
-  internals,
-  origin,
-  measures,
-  viewbox,
-  points,
-  lines,
-  multilinestrings,
-  multipolygons,
+  return Doc.vsep([
+    Doc.vsep(
+      Record.toEntries(_names).map(([k, v]) =>
+        Doc.text(`import ${v} from './${k}'`)
+      )
+    ),
+    Doc.line,
+    Doc.text(`export const mapData = {`),
+    Doc.indent(
+      Doc.vsep(Record.toEntries(_names).map(([k, _v]) => Doc.text(`${k},`))),
+      2
+    ),
+    Doc.text(`}`),
+  ])
 }
-`)
+
+export const convName = (
+  _type: _Type
+): Effect.Effect<void, PlatformError, FileSystem.FileSystem> =>
+  Effect.gen(function* () {
+    const fs = yield* FileSystem.FileSystem
+    const jsonStr = yield* fs.readFileString(`./${_type}.json`)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const jsonObj = JSON.parse(jsonStr)
+    const geojson = decodeGeoJSON(jsonObj)
+    const doc = printGeoJsonAsTs(_type, _types[_type], geojson)
+    const ts = Doc.render(doc, { style: 'pretty' })
+    yield* fs.writeFileString(`./${_type}.ts`, ts)
+  })
+
+// eslint-disable-next-line functional/functional-parameters
+export const convNames = (): Effect.Effect<
+  void[],
+  PlatformError,
+  FileSystem.FileSystem
+> => Effect.all(Record.keys(_types).map(convName))
+
+// eslint-disable-next-line functional/functional-parameters
+export const saveAllTs = (): Effect.Effect<
+  void,
+  PlatformError,
+  FileSystem.FileSystem
+> =>
+  Effect.gen(function* () {
+    const fs = yield* FileSystem.FileSystem
+    const doc = printAllTs()
+    const ts = Doc.render(doc, { style: 'pretty' })
+    yield* fs.writeFileString(`./all.ts`, ts)
+  })
+
+////
+
+const _types = {
+  areas: 'MultiPolygonGeoJSON',
+  internals: 'MultiPolygonGeoJSON',
+  origin: 'PointGeoJSON',
+  measures: 'LineGeoJSON<MeasureProperties>',
+  viewbox: 'LineGeoJSON',
+
+  'map-points': 'OsmPointGeoJSON',
+  'map-lines': 'OsmLineGeoJSON',
+  'map-multilinestrings': 'OsmMultiLineStringGeoJSON',
+  'map-multipolygons': 'OsmMultiPolygonGeoJSON',
+}
+
+type _Type = keyof typeof _types
+
+const _names = {
+  areas: 'areas',
+  internals: 'internals',
+  origin: 'origin',
+  measures: 'measures',
+  viewbox: 'viewbox',
+
+  'map-points': 'points',
+  'map-lines': 'lines',
+  'map-multilinestrings': 'multilinestrings',
+  'map-multipolygons': 'multipolygons',
 }
