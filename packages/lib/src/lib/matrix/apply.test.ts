33046a599f4906abf31066acd1e16c89b8d0c2c2
Masao Uebayashi <uebayasi@tombiinc.com>
2025-12-29 23:12:34 +0900

    convert more

--- packages/lib/src/lib/matrix/apply.test.ts
+++ packages/lib/src/lib/matrix/apply.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from 'vitest'
+import { expect, test } from '@rstest/core'
 import { type V } from '../tuple'
 import { apply } from './apply'
 import { type Matrix } from './main'
