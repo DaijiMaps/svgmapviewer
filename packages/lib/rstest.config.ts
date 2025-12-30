ee9bcccfec62cb206b00f1f5d5f8b69724412b8b
Masao Uebayashi <uebayasi@tombiinc.com>
2025-12-30 21:16:34 +0900

    explicitly exclude browser tests

--- packages/lib/rstest.config.ts
+++ packages/lib/rstest.config.ts
@@ -3,4 +3,5 @@ import { defineConfig } from '@rstest/core'
 export default defineConfig({
   testEnvironment: 'node',
   include: ['tests/unit/**/*.test.{js,ts}'],
+  exclude: ['tests/browser'],
 })
