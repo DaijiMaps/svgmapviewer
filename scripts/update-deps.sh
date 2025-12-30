c411e3fb2c45b597c425cbd5d80c9f118982b23a
Masao Uebayashi <uebayasi@tombiinc.com>
2025-12-30 13:19:11 +0900

    chore: pnpm scripts

---
+++ scripts/update-deps.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+branch=$(git branch --show-current)
+if [ "$branch" != "chore/deps" ]; the
+	echo >&2 'run this script only on chore/deps!'
+	exit 0
+fi
+
+pnpm -r up --latest
+pnpx prettier -w pnpm-lock.yaml
+git add pnpm-lock.yaml
