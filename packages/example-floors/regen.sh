b7c720d7aa4b7803a4dc7114c9f1b8a81b05ed4d
Masao Uebayashi <uebayasi@tombiinc.com>
2026-01-05 22:22:41 +0900

    update floor example svg

--- packages/example-floors/regen.sh
+++ packages/example-floors/regen.sh
@@ -2,5 +2,6 @@
 cd src/assets
 for x in 1 2; do
   inkscape -jl -i layer${x} -o floor-${x}f.svg floors.inkscape.svg
+  perl -0777 -pi -le "s/font-family:[^;]+?;/font-family:'Ubuntu Sans',sans-serif;/gmos" floor-${x}f.svg
 done
 )
