ea7c74ab20ddef4690ae14070a9000f59ad1f555
Masao Uebayashi <uebayasi@tombiinc.com>
2026-01-10 21:57:48 +0900

    fix: ui: hide floor menu

--- packages/lib/src/lib/ui/Left.tsx
+++ packages/lib/src/lib/ui/Left.tsx
@@ -6,9 +6,13 @@ import {
   flex_row_center_center,
   pointer_events_none,
   position_absolute_left_0_top_0,
+  timing_closing,
+  timing_opening,
+  ZOOM_DURATION_HEADER,
 } from '../css'
 import { useShadowRoot } from '../dom'
 import { Floors } from './Floor'
+import { useOpenCloseHeader } from './ui-react'
 
 export function Left(): ReactNode {
   useShadowRoot('left', <LeftRoot />, 'ui')
@@ -20,7 +24,10 @@ function LeftRoot(): ReactNode {
   return (
     <div className="ui-content left bottom">
       <Floors />
-      <style>{style}</style>
+      <style>
+        {style}
+        <LeftStyle />
+      </style>
     </div>
   )
 }
@@ -32,16 +39,58 @@ const style = `
   padding: 0.4em;
   font-size: smaller;
   ${pointer_events_none}
-
-  transform-origin: 100% 50%;
-  transform: translateY(calc(-50% + 50vh));
-  transform: translateY(calc(-50% + 50svh));
 }
 
 .left {
   top: initial;
   align-items: end;
-
-  transform-origin: 100% 100%;
 }
 `
+
+export function LeftStyle(): ReactNode {
+  const { open, animating } = useOpenCloseHeader()
+
+  if (!animating) {
+    const b = !open ? 0 : 1
+
+    return (
+      <>{`
+.left {
+  --b: ${b};
+  transform-origin: 0% 50%;
+  opacity: var(--b);
+  transform: translate(0%, calc(50vh - 50%)) scale(var(--b));
+  will-change: opacity, transform;
+}
+`}</>
+    )
+  } else {
+    const [a, b] = !open ? [1, 0] : [0, 1]
+    const t = open ? timing_opening : timing_closing
+
+    return (
+      <>{`
+.left {
+  --timing: ${t};
+  --duration: ${ZOOM_DURATION_HEADER}ms;
+  --a: ${a};
+  --b: ${b};
+  transform-origin: 0% 50%;
+  animation: xxx-left var(--duration) var(--timing);
+  will-change: opacity, transform;
+}
+
+@keyframes xxx-left {
+  from {
+    opacity: var(--a);
+    transform: translate(0%, calc(50vh - 50%)) scale(var(--a)) translate3d(0px, 0px, 0px);
+  }
+  to {
+    opacity: var(--b);
+    transform: translate(0%, calc(50vh - 50%)) scale(var(--b)) translate3d(0px, 0px, 0px);
+  }
+}
+`}</>
+    )
+  }
+}
