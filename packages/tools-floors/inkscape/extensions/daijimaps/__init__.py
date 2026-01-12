e58176fb4e105796aba685faa03bb3d1afb61b9a
Masao Uebayashi <uebayasi@tombiinc.com>
2026-01-12 16:56:34 +0900

    wip

--- packages/tools-floors/inkscape/extensions/daijimaps/__init__.py
+++ packages/tools-floors/inkscape/extensions/daijimaps/__init__.py
@@ -1,3 +1,3 @@
 from .address_tree import AddressTree, GenerateAddresses, SaveAddresses
-from .draw import draw_shop_name, read_shop_name, preferInt
+from .name import draw_shop_name, read_shop_name, preferInt
 from .renumber import renumber_group
