41855b12dee86650d68f4ee7a6bbbcba91951574
Masao Uebayashi <uebayasi@tombiinc.com>
2026-01-23 21:09:21 +0900

    fix build (pos -> coord)

--- packages/lib/src/lib/search/types.ts
+++ packages/lib/src/lib/search/types.ts
@@ -1,14 +1,12 @@
 import type Flatbush from 'flatbush'
 
-import { type VecVec } from '../vec/prefixed'
+import { type Addr, type Pos } from '../address'
 
-export type AddressString = string
 export type Idx = string
 
 export interface SearchPos {
-  address: AddressString
-  coord: VecVec
-  fidx: number
+  address: Addr
+  pos: Pos
 }
 
 ////
