4af027c801a6d1a108a7d6bd6591ebc4cece2e1e
Masao Uebayashi <uebayasi@tombiinc.com>
2026-05-14 02:56:08 +0900

    g/c svgpatheditor

--- packages/svgpatheditor/src/lib/canvas/utils.ts
+++
@@ -1,7 +0,0 @@
-import type { VecVec } from 'svgmapviewer/vec'
-
-import { boxBox, type BoxBox } from 'svgmapviewer/box'
-
-export function sizeToViewBox(size: VecVec): BoxBox {
-  return boxBox(-size.x / 2, -size.y / 2, size.x, size.y)
-}
