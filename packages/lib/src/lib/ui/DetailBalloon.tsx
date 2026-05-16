1e617102d482aed6151c56394b0d0d8e35140773
Masao Uebayashi <uebayasi@tombiinc.com>
2026-05-16 22:35:22 +0900

    refactor

--- packages/lib/src/lib/ui/DetailBalloon.tsx
+++ packages/lib/src/lib/ui/DetailBalloon.tsx
@@ -40,6 +40,48 @@ function BalloonDetailStyle() {
 }
 
 const style = `
+.detail.not-animating {
+  transform-origin: 0 0;
+  transform: translate(var(--tx1-x), var(--tx1-y)) scale(var(--s-b)) translate(-50%, -50%) translate3d(0px, 0px, 0px);
+}
+.balloon.not-animating {
+  transform-origin: 0 0;
+  transform: translate(var(--tx1-x), var(--tx1-y)) scale(var(--s-b)) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
+}
+
+.detail.animating,
+.balloon.animating {
+  transform-origin: 0 0;
+  will-change: opacity, transform;
+}
+.detail.animating {
+  animation: xxx-detail var(--duration) var(--timing);
+}
+.balloon.animating {
+  animation: xxx-balloon var(--duration) var(--timing);
+}
+
+@keyframes xxx-detail {
+  from {
+    opacity: var(--o-a);
+    transform: translate(var(--tx1-a-x), var(--tx1-a-y)) scale(var(--s-a)) translate(-50%, -50%) translate3d(0px, 0px, 0px);
+  }
+  to {
+    opacity: var(--o-b);
+    transform: translate(var(--tx1-b-x), var(--tx1-b-y)) scale(var(--s-b)) translate(-50%, -50%) translate3d(0px, 0px, 0px);
+  }
+}
+
+@keyframes xxx-balloon {
+  from {
+    opacity: var(--o-a);
+    transform: translate(var(--tx1-a-x), var(--tx1-a-y)) scale(var(--s-a)) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
+  }
+  to {
+    opacity: var(--o-b);
+    transform: translate(var(--tx1-b-x), var(--tx1-b-y)) scale(var(--s-b)) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
+  }
+}
 `
 
 function Assets() {
--- packages/lib/src/lib/ui/balloon-common.ts
+++ packages/lib/src/lib/ui/balloon-common.ts
@@ -212,18 +212,11 @@ export function balloonStyle(
     return `
 .detail.not-animating,
 .balloon.not-animating {
+  --s-b: ${s.b};
+  --tx1-x: ${tx1.x}px;
+  --tx1-y: ${tx1.y}px;
   --pww: ${-width / 2}px;
   --phh: ${-height / 2}px;
-}
-
-.detail.not-animating {
-  transform-origin: 0 0;
-  transform: translate(${tx1.x}px, ${tx1.y}px) scale(${s.b}) translate(-50%, -50%) translate3d(0px, 0px, 0px);
-}
-
-.balloon.not-animating {
-  transform-origin: 0 0;
-  transform: translate(${tx1.x}px, ${tx1.y}px) scale(${s.b}) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
 }
 `
   } else {
@@ -248,38 +241,6 @@ export function balloonStyle(
   --tx1-b-y: ${tx1.b.y}px;
   --pww: ${-width / 2}px;
   --phh: ${-height / 2}px;
-  transform-origin: 0 0;
-  will-change: opacity, transform;
-}
-
-.detail.animating {
-  animation: xxx-detail var(--duration) var(--timing);
-}
-
-.balloon.animating {
-  animation: xxx-balloon var(--duration) var(--timing);
-}
-
-@keyframes xxx-detail {
-  from {
-    opacity: var(--o-a);
-    transform: translate(var(--tx1-a-x), var(--tx1-a-y)) scale(var(--s-a)) translate(-50%, -50%) translate3d(0px, 0px, 0px);
-  }
-  to {
-    opacity: var(--o-b);
-    transform: translate(var(--tx1-b-x), var(--tx1-b-y)) scale(var(--s-b)) translate(-50%, -50%) translate3d(0px, 0px, 0px);
-  }
-}
-
-@keyframes xxx-balloon {
-  from {
-    opacity: var(--o-a);
-    transform: translate(var(--tx1-a-x), var(--tx1-a-y)) scale(var(--s-a)) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
-  }
-  to {
-    opacity: var(--o-b);
-    transform: translate(var(--tx1-b-x), var(--tx1-b-y)) scale(var(--s-b)) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
-  }
 }
 `
   }
