29569703dc947d8d68698164c5748e5b1eaf0376
Masao Uebayashi <uebayasi@tombiinc.com>
2026-05-17 11:25:54 +0900

    refactor

--- packages/lib/src/lib/ui/DetailBalloon.tsx
+++ packages/lib/src/lib/ui/DetailBalloon.tsx
@@ -5,7 +5,7 @@ import { useMemo, type ReactNode } from 'react'
 import { RenderMapAssetsDefault } from '../carto/assets'
 import { useShadowRoot } from '../dom'
 import { Balloon } from './Balloon'
-import { calcBalloonLayout } from './balloon-common'
+import { calcBalloonLayout, detailStyleString } from './balloon-common'
 import { Detail } from './Detail'
 import { useDetail } from './ui-react'
 
@@ -33,55 +33,10 @@ function BalloonDetailStyle() {
     <>
       <Balloon {...props} />
       <Detail {...props} _detail={detail} />
-      <style>{style}</style>
+      <style>{detailStyleString}</style>
     </>
   )
 }
-
-const style = `
-.detail.not-animating {
-  transform-origin: 0 0;
-  transform: translate(var(--tx1-x), var(--tx1-y)) scale(var(--s-b)) translate(-50%, -50%) translate3d(0px, 0px, 0px);
-}
-.balloon.not-animating {
-  transform-origin: 0 0;
-  transform: translate(var(--tx1-x), var(--tx1-y)) scale(var(--s-b)) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
-}
-
-.detail.animating,
-.balloon.animating {
-  transform-origin: 0 0;
-  will-change: opacity, transform;
-}
-.detail.animating {
-  animation: xxx-detail var(--duration) var(--timing);
-}
-.balloon.animating {
-  animation: xxx-balloon var(--duration) var(--timing);
-}
-
-@keyframes xxx-detail {
-  from {
-    opacity: var(--o-a);
-    transform: translate(var(--tx1-a-x), var(--tx1-a-y)) scale(var(--s-a)) translate(-50%, -50%) translate3d(0px, 0px, 0px);
-  }
-  to {
-    opacity: var(--o-b);
-    transform: translate(var(--tx1-b-x), var(--tx1-b-y)) scale(var(--s-b)) translate(-50%, -50%) translate3d(0px, 0px, 0px);
-  }
-}
-
-@keyframes xxx-balloon {
-  from {
-    opacity: var(--o-a);
-    transform: translate(var(--tx1-a-x), var(--tx1-a-y)) scale(var(--s-a)) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
-  }
-  to {
-    opacity: var(--o-b);
-    transform: translate(var(--tx1-b-x), var(--tx1-b-y)) scale(var(--s-b)) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
-  }
-}
-`
 
 function Assets() {
   return (
--- packages/lib/src/lib/ui/balloon-common.ts
+++ packages/lib/src/lib/ui/balloon-common.ts
@@ -193,6 +193,55 @@ export function balloonPaths(
   }
 }
 
+export const detailStyleString = `
+.not-animating {
+  &.detail {
+    transform-origin: 0 0;
+    transform: translate(var(--tx1-x), var(--tx1-y)) scale(var(--s-b)) translate(-50%, -50%) translate3d(0px, 0px, 0px);
+  }
+  &.balloon {
+    transform-origin: 0 0;
+    transform: translate(var(--tx1-x), var(--tx1-y)) scale(var(--s-b)) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
+  }
+}
+
+.animating {
+  &.detail,
+  &.balloon {
+    transform-origin: 0 0;
+    will-change: opacity, transform;
+  }
+  &.detail {
+    animation: xxx-detail var(--duration) var(--timing);
+  }
+  &.balloon {
+    animation: xxx-balloon var(--duration) var(--timing);
+  }
+}
+
+@keyframes xxx-detail {
+  from {
+    opacity: var(--o-a);
+    transform: translate(var(--tx1-a-x), var(--tx1-a-y)) scale(var(--s-a)) translate(-50%, -50%) translate3d(0px, 0px, 0px);
+  }
+  to {
+    opacity: var(--o-b);
+    transform: translate(var(--tx1-b-x), var(--tx1-b-y)) scale(var(--s-b)) translate(-50%, -50%) translate3d(0px, 0px, 0px);
+  }
+}
+
+@keyframes xxx-balloon {
+  from {
+    opacity: var(--o-a);
+    transform: translate(var(--tx1-a-x), var(--tx1-a-y)) scale(var(--s-a)) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
+  }
+  to {
+    opacity: var(--o-b);
+    transform: translate(var(--tx1-b-x), var(--tx1-b-y)) scale(var(--s-b)) translate(var(--pww), var(--phh)) translate3d(0px, 0px, 0px);
+  }
+}
+`
+
 export function useDetailStyle(
   ref: Readonly<RefObject<HTMLDivElement | null>>,
   Q: null | V,
