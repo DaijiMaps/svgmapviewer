e92ae9d461ab11359d9fe40a26ef708d2c630f27
Masao Uebayashi <uebayasi@tombiinc.com>
2026-05-29 01:47:39 +0900

    checkpoint first working example-floors with astro

---
+++ packages/astro-example-floors/src/apps/data/pois.ts
@@ -0,0 +1,134 @@
+import { type POI } from '../types.ts'
+
+export const pois: POI[] = [
+  {
+    id: 0,
+    name: 'a',
+    coord: { x: 10, y: 10 },
+    size: 10,
+    fidx: 0,
+    x: {
+      tag: 'facility',
+      kind: {
+        tag: 'toilet',
+      },
+    },
+  },
+  {
+    id: 1,
+    name: ['フードコート'],
+    coord: { x: 100, y: 100 },
+    size: 10,
+    fidx: 0,
+    x: {
+      tag: 'shop',
+      kind: {
+        tag: 'restaurant',
+      },
+    },
+  },
+  {
+    id: 2,
+    name: ['ラーメン'],
+    coord: { x: 100, y: 30 },
+    size: 10,
+    fidx: 0,
+    x: {
+      tag: 'shop',
+      kind: {
+        tag: 'restaurant',
+      },
+    },
+  },
+  {
+    id: 3,
+    name: ['寿司'],
+    coord: { x: 140, y: 40 },
+    size: 10,
+    fidx: 0,
+    x: {
+      tag: 'shop',
+      kind: {
+        tag: 'restaurant',
+      },
+    },
+  },
+  {
+    id: 3,
+    name: ['パスタ'],
+    coord: { x: 150, y: 70 },
+    size: 10,
+    fidx: 0,
+    x: {
+      tag: 'shop',
+      kind: {
+        tag: 'restaurant',
+      },
+    },
+  },
+  {
+    id: 4,
+    name: ['ハンバーガー'],
+    coord: { x: 180, y: 110 },
+    size: 10,
+    fidx: 0,
+    x: {
+      tag: 'shop',
+      kind: {
+        tag: 'restaurant',
+      },
+    },
+  },
+  {
+    id: 5,
+    name: ['クレープ'],
+    coord: { x: 190, y: 140 },
+    size: 10,
+    fidx: 0,
+    x: {
+      tag: 'shop',
+      kind: {
+        tag: 'restaurant',
+      },
+    },
+  },
+  {
+    id: 6,
+    name: ['パン'],
+    coord: { x: 90, y: 140 },
+    size: 10,
+    fidx: 0,
+    x: {
+      tag: 'shop',
+      kind: {
+        tag: 'restaurant',
+      },
+    },
+  },
+  {
+    id: 201,
+    name: '書店',
+    coord: { x: 100, y: 50 },
+    size: 10,
+    fidx: 1,
+    x: {
+      tag: 'shop',
+      kind: {
+        tag: 'book',
+      },
+    },
+  },
+  {
+    id: 202,
+    name: 'd',
+    coord: { x: 200, y: 200 },
+    size: 10,
+    fidx: 1,
+    x: {
+      tag: 'facility',
+      kind: {
+        tag: 'stairs',
+      },
+    },
+  },
+]
---
+++ packages/astro-example-floors/src/apps/viewbox.ts
@@ -0,0 +1,4 @@
+import origBoundingBox from './data/bbox.json' with { type: 'json' }
+import origViewBox from './data/viewbox.json' with { type: 'json' }
+
+export { origViewBox, origBoundingBox }
