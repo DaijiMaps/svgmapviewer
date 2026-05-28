2491c75bb06c3c843da785d67981644279ecb45e
Masao Uebayashi <uebayasi@tombiinc.com>
2026-05-28 23:16:47 +0900

    lib: make cb capable of async cb

--- packages/lib/src/lib/cb.ts
+++ packages/lib/src/lib/cb.ts
@@ -1,15 +1,18 @@
 /* eslint-disable functional/no-expression-statements */
 /* eslint-disable functional/no-return-void */
 
-export type Cb = () => void
+export type Cb = () => void | Promise<void>
 export type Cbs = Set<Cb>
 
-export type Cb1<T> = (args: T) => void
+export type Cb1<T> = (args: T) => void | Promise<void>
 export type Cbs1<T> = Set<Cb1<T>>
 
-export function notifyCbs0(cbs: Readonly<Set<Cb>>): void {
-  cbs.forEach((cb: Cb) => cb())
+export function notifyCbs0(cbs: Readonly<ReadonlySet<Cb>>): void {
+  Promise.all(cbs.values().map((cb) => Promise.resolve(cb())))
 }
-export function notifyCbs<T>(cbs: Readonly<Set<Cb1<T>>>, args: T): void {
-  cbs.forEach((cb: Cb1<T>) => cb(args))
+export function notifyCbs<T>(
+  cbs: Readonly<ReadonlySet<Cb1<T>>>,
+  args: T
+): void {
+  Promise.all(cbs.values().map((cb) => Promise.resolve(cb(args))))
 }
