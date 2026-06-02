0055dc41cb0f3893c41771fad539531e0b605bd7
Masao Uebayashi <uebayasi@tombiinc.com>
2026-06-02 14:41:47 +0900

    refactor

--- packages/example-floors-astro/src/utils/data.ts
+++ packages/example-floors-astro/src/utils/data.ts
@@ -16,28 +16,26 @@ const toAddress = (
   address: x.id,
   floorPos: x.data,
 })
-
 const toName = (x: Readonly<CollectionEntry<'names'>>): SearchName => ({
   name: x.id,
   addresses: x.data,
 })
-
 const toInfo = (x: Readonly<CollectionEntry<'pois'>>): SearchInfo => ({
   name: x.id,
   info: { title: x.id, x: x.data },
 })
 
-////
-
-const tmpConfig = await e('svgMapViewerConfig', 'default')
-const tmpFloors = await e('floors', 'default')
+const svgMapViewerConfig = await e('svgMapViewerConfig', 'default')?.then(
+  (x) => x?.data
+)
+const floorsConfig = await e('floors', 'default')?.then((x) => x?.data)
 const searchAddresses = await c('addresses').then((a) => a.map(toAddress))
 const searchNames = await c('names').then((a) => a.map(toName))
 const searchInfos = await c('pois').then((a) => a.map(toInfo))
 
 export const svgMapViewerConfigUser: SvgMapViewerConfigUser = {
-  ...tmpConfig?.data,
-  floorsConfig: tmpFloors?.data,
+  ...svgMapViewerConfig,
+  floorsConfig,
   searchAddresses,
   searchNames,
   searchInfos,
