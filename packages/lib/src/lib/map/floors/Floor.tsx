ef5c0b01fc51b72279f3abdd049e5fc1b368eb05
Masao Uebayashi <uebayasi@tombiinc.com>
2026-06-13 11:55:15 +0900

    lib: refactor floor label handling

--- packages/lib/src/lib/map/floors/Floor.tsx
+++ packages/lib/src/lib/map/floors/Floor.tsx
@@ -1,6 +1,12 @@
 /* eslint-disable functional/no-expression-statements */
 /* eslint-disable functional/functional-parameters */
-import { Fragment, useRef, type PropsWithChildren, type ReactNode } from 'react'
+import {
+  Fragment,
+  useRef,
+  type CSSProperties,
+  type PropsWithChildren,
+  type ReactNode,
+} from 'react'
 
 import {
   type Floor,
@@ -11,13 +17,13 @@ import {
 import { type BoxBox } from '../../box/prefixed'
 import type { Cb } from '../../cb'
 import { floor_appearing_animation } from '../../css'
-import { useStyleRef } from '../../style/ref'
 import { useLayout2 } from '../../style/style-react'
 import {
   useFloors,
   type UseFloorsReturn,
 } from '../../viewer/floors/floors-react'
 import { useFloorRef } from '../../viewer/floors/style'
+import { useAnimationStyleRef } from '../../viewer/layout/style'
 import { MAP_SVG_FLOORS } from '../map-svg-react'
 
 export function RenderFloors(props: Readonly<OsmRenderMapProps>): ReactNode {
@@ -247,13 +253,15 @@ text, tspan {
 }
 */
 
-function RenderFloorHtmlLabels({ idx, labels }: Props): ReactNode {
+function RenderFloorHtmlLabels({ idx: fidx, labels }: Props): ReactNode {
+  const ref = useRef(null)
+  useAnimationStyleRef(ref, `labels-${fidx}`)
   return (
-    <div className="labels">
-      {(labels ?? []).map((_text, lidx) => {
+    <div ref={ref} className="labels">
+      {labels?.map((_text, idx) => {
         return (
-          <Fragment key={lidx}>
-            <RenderFloorHtmlLabel _text={_text} fidx={idx} idx={lidx} />
+          <Fragment key={idx}>
+            <RenderFloorHtmlLabel _text={_text} />
           </Fragment>
         )
       })}
@@ -262,57 +270,48 @@ function RenderFloorHtmlLabels({ idx, labels }: Props): ReactNode {
   )
 }
 
-const labelRefs = new Map()
-
-function RenderFloorHtmlLabel({
-  fidx,
-  _text,
-  idx,
-}: Readonly<{
-  fidx: number
+type LabelProps = Readonly<{
   _text: LabelText
-  idx: number
-}>): ReactNode {
-  const ref = useRef(null)
-  useStyleRef(labelRefs, ref, `f${fidx}-label-${idx}`)
+}>
+
+function RenderFloorHtmlLabel({ _text }: LabelProps): ReactNode {
   return (
     <div
-      style={{
-        transform: `
-translate(${_text.attrs?.['x'] || 0}px,${_text.attrs?.['y'] || 0}px)
-`,
-      }}
+      className="label"
+      style={
+        {
+          '--x': (_text.attrs?.['x'] || 0) + 'px',
+          '--y': (_text.attrs?.['y'] || 0) + 'px',
+          '--rotate': (_text.attrs?.['rotate'] || 0) + 'deg',
+          '--scale2': (_text.attrs?.['scale2'] || 1) + '',
+          '--scale1': (_text.attrs?.['scale1'] || 1) + '',
+        } as CSSProperties
+      }
     >
-      <div
-        ref={ref}
-        className="label"
-        style={{
-          transform: `
-rotate(${_text.attrs?.['rotate'] || 0}deg)
-scale(${_text.attrs?.['scale2'] || 0})
-scale(${_text.attrs?.['scale1'] || 0})
-translate(-50%, -50%)
-`,
-        }}
-      >
-        {'id' in _text.attrs && 'style' in _text.attrs && (
-          <style>{`#${_text.attrs['id']} { ${_text.attrs['style']}; }`}</style>
-        )}
-        {_text.children &&
-          _text.children.map((_tspan, idx2) => (
-            <p key={idx2} {...fromAttrs(_tspan.attrs)}>
-              {_tspan.text ?? ''}
-            </p>
-          ))}
-      </div>
+      {_text.children?.map((_tspan, idx2) => (
+        <p key={idx2} {...fromAttrs(_tspan.attrs)}>
+          {_tspan.text ?? ''}
+        </p>
+      ))}
     </div>
   )
 }
 
 const htmlLabelsStyle = `
+@property --zoom {
+  syntax: '<number>';
+  inherits: false;
+  initial-value: 1;
+}
+div.labels {
+  /* default */
+  --zoom: 1;
+  --zoom-zoom: 1;
+}
 div.label {
   position: absolute;
   transform-origin: left top;
+  transform: translate(var(--x), var(--y)) rotate(var(--rotate)) scale(var(--zoom)) scale(calc(1 / var(--zoom-zoom))) scale(var(--scale2)) scale(var(--scale1)) translate(-50%, -50%);
   text-align: center;
   font-family: 'Noto Sans JP', 'Noto Sans', 'sans-serif' !important;
   font-weight: 200 !important;
@@ -320,6 +319,19 @@ div.label {
     margin: 0;
   }
 }
+/*
+div.labels.zooming > div.label {
+  animation: xxx-label-scale 500ms ease;
+}
+@keyframes xxx-label-scale {
+  from {
+    --zoom: 1;
+  }
+  to {
+    --zoom: var(--zoom-zq-inv);
+  }
+}
+*/
 `
 
 // XXX check if all urls are loaded?
